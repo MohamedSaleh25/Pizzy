365625da99c05c0a4977ba9da95d05cc24cf457f
Mohamed Saleh <meamohamed170@gmail.com>
2026-05-27 10:17:03 +0300

    update settings

--- project/settings.py
+++ project/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-#ALLOWED_HOSTS = [".ngrok-free.app", "127.0.0.1", "localhost"]
-#CSRF_TRUSTED_ORIGINS = ["https://*.ngrok-free.app"]
+#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [".ngrok-free.app", "127.0.0.1", "localhost"]
+CSRF_TRUSTED_ORIGINS = ["https://*.ngrok-free.app"]
 
 # Application definition
 
