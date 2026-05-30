b3bbe240b08e42f00c635befaf4be01330505e32
Mohamed Saleh <meamohamed170@gmail.com>
2026-05-30 04:52:48 +0300

    fix bootsnav static path

--- project/settings.py
+++ project/settings.py
@@ -143,11 +143,18 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = [
+ #   os.path.join(BASE_DIR, 'project/static'),
+#]   
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'project/static'),
-]   
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
